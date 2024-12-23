d76b44b29123c522df699a682b8130048ccdadcd
Kirill Zubkov <kirill131313@gmail.com>
2024-12-23 13:01:18 +0300

    Обновление исключений

--- src/main/java/com/example/walletapp/exception/InsufficientFundsException.java
+++ src/main/java/com/example/walletapp/exception/InsufficientFundsException.java
@@ -1,5 +1,9 @@
 package com.example.walletapp.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InsufficientFundsException extends RuntimeException {
     public InsufficientFundsException(String message) {
         super(message);
